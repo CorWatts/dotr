f2a7dd7a298db3a5458e0c008784816144c2e90f
Corey Watts <cwatts@brightidea.com>
2015-11-07 17:19:30 -0800

    Deleting leftover cruft

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -5,50 +5,6 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet', '$mdDialog', function($sc
 		$scope.showSearch = !$scope.showSearch;
 	};
 
-	// Mock activity
-	$scope.activity = [
-		{
-			what: 'Brunch this weekend?',
-			who: 'Ali Conners',
-			avatar: 'svg-1',
-			when: '3:08PM',
-			notes: " I'll be in your neighborhood doing errands",
-			image: "//i.imgur.com/PtlR97G.gif"
-		},
-		{
-			what: 'Summer BBQ',
-			who: 'to Alex, Scott, Jennifer',
-			avatar: 'svg-2',
-			when: '3:08PM',
-			notes: "Wish I could come out but I'm out of town this weekend",
-			image: "//i.imgur.com/PtlR97G.gif"
-		},
-		{
-			what: 'Oui Oui',
-			who: 'Sandra Adams',
-			avatar: 'svg-3',
-			when: '3:08PM',
-			notes: "Do you have Paris recommendations? Have you ever been?",
-			image: "//i.imgur.com/PtlR97G.gif"
-		},
-		{
-			what: 'Birthday Gift',
-			who: 'Trevor Hansen',
-			avatar: 'svg-4',
-			when: '3:08PM',
-			notes: "Have any ideas of what we should get Heidi for her birthday?",
-			image: "//i.imgur.com/PtlR97G.gif"
-		},
-		{
-			what: 'Recipe to try',
-			who: 'Brian Holt',
-			avatar: 'svg-5',
-			when: '3:08PM',
-			notes: "We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos",
-			image: "//i.imgur.com/PtlR97G.gif"
-		}
-	];
-
 	// Bottomsheet & Modal Dialogs
 	$scope.alert = '';
 	$scope.showListBottomSheet = function($event) {
